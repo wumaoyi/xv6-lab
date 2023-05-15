cfc954590d28b78694ba402346f5d6ce3581ec73
wmy <1330521979@qq.com>
2023-05-15 20:12:36 +0800

    finsh sleep

--- user/sleep.c
+++ user/sleep.c
@@ -4,7 +4,7 @@
 
 int main(int argc , char* argv[]){
     if(argc <= 1){
-        fprint(2 , "usage sleep second\n");
+        fprintf(2 , "usage sleep second\n");
         exit(1);
     }
     sleep(atoi(argv[0]));
