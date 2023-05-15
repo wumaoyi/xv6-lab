799a8f29f871787531254f8e8a90de102d73996e
wmy <1330521979@qq.com>
2023-05-15 20:10:25 +0800

    finsh sleep

---
+++ user/sleep.c
@@ -0,0 +1,12 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc , char* argv[]){
+    if(argc <= 1){
+        fprint(2 , "usage sleep second\n");
+        exit(1);
+    }
+    sleep(atoi(argv[0]));
+    exit(0);
+}
