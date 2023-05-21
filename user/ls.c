1f397b55e9c625c786db99ef6a903363339c7f29
wmy <1330521979@qq.com>
2023-05-21 14:02:19 +0800

    3

--- user/ls.c
+++ user/ls.c
@@ -40,7 +40,7 @@ ls(char *path)
     close(fd); 
     return;
   }
-  printf("%s %d\n" , path , st.type);
+  //printf("%s %d\n" , path , st.type);
   switch(st.type){
   case T_FILE:
     printf("%s %d %d %l\n", fmtname(path) ,st.type, st.ino, st.size);
@@ -63,7 +63,7 @@ ls(char *path)
         printf("ls: cannot stat %s\n", buf);
         continue;
       }
-      printf("%s %d %d %d\n", buf , st.type, st.ino, st.size);
+      printf("%s %d %d %d\n", fmtname(buf) , st.type, st.ino, st.size);
     }
     break;
   }
