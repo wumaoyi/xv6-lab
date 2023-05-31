358326990c3920d19f883ed070dd82bbfb29c5e6
wmy <1330521979@qq.com>
2023-05-31 15:00:41 +0800

    4

--- Makefile
+++ Makefile
@@ -194,7 +194,6 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_trace\
-	$U/_sysinfo\
 	$U/_sysinfotest\
 
 
--- kernel/trap.c
+++ kernel/trap.c
@@ -43,7 +43,7 @@ usertrap(void)
 
   // send interrupts and exceptions to kerneltrap(),
   // since we're now in the kernel.
-  w_stvec((uint64)kernelvec);
+  w_stvec((uint64)kernelvec);//
 
   struct proc *p = myproc();
   
