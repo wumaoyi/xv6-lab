e77c35a76c4be0a0049225be6b9f78f28d4d9e06
wmy <1330521979@qq.com>
2023-06-05 16:23:41 +0800

    finish

--- kernel/proc.c
+++ kernel/proc.c
@@ -213,7 +213,7 @@ proc_pagetable(struct proc *p)
   // todo usyscall below TRAPFRAME #define USYSCALL (TRAPFRAME - PGSIZE)
   if(mappages(pagetable, USYSCALL, PGSIZE,
               (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
-    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
     uvmfree(pagetable, 0);
     return 0;
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -80,7 +80,41 @@ sys_sleep(void)
 int
 sys_pgaccess(void)
 {
+  /*
+  kernel/vm.c 中的 walk（） 对于查找正确的 PTE 非常有用。
+  您需要在kernel/riscv.h中定义PTE_A，即访问位。请参阅RISC-V手册以确定其值。
+  检查是否设置后，请务必清除PTE_A。否则，将无法确定自上次调用 pgaccess（） 以来是否访问了该页面（即，该位将永久设置）。
+  vmprint（） 在调试页表时可能会派上用场。
+  */
   // lab pgtbl: your code here.
+  //todo 
+  //1  你需要使用 argaddr（） 和 argint（） 解析参数。
+  uint64 addr;
+  int len;
+  int bitmask;
+  if(argaddr(0, &addr) < 0)
+    return -1;
+  if(argint(1, &len) < 0)
+    return -1;
+  if(argint(2, &bitmask) < 0)
+    return -1;
+
+//  可以设置可以扫描的页数上限。    
+  if(len > 32 || len <0){
+    return -1;
+  }  
+  struct proc *p = myproc();//获取当前进程   
+  int res = 0;  
+  for(int i = 0 ; i < len ; ++i){
+    uint64 va = addr + i*PGSIZE;
+    int abit = vm_pgaccess(p->pagetable , va);
+    res = res | (abit << i);
+  }
+  // 2 对于输出位掩码，在内核中存储临时缓冲区并在用正确的位填充后将其复制给用户（通过 copyout（））会更容易。
+
+  if(copyout(p->pagetable , bitmask , (char*)&res,sizeof(res)) < 0){ // 从kernel复制数据给user
+    return -1;
+  }  
   return 0;
 }
 #endif
